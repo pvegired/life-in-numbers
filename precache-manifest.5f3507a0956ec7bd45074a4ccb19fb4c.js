fe0e8849f8e46e247b7654362cb383e53b8468e3
pradeep <pvegired@andrew.cmu.edu>
2021-12-04 01:48:27 -0500

    Updates

---
+++ precache-manifest.5f3507a0956ec7bd45074a4ccb19fb4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5edbf59d2b4d9b924f0429dc1e7ff10b",
+    "url": "/life-in-numbers/index.html"
+  },
+  {
+    "revision": "ddb76732321fc60d4cd4",
+    "url": "/life-in-numbers/static/css/2.cda93978.chunk.css"
+  },
+  {
+    "revision": "d5cfb51ff7d45924c55c",
+    "url": "/life-in-numbers/static/css/main.e2c55e50.chunk.css"
+  },
+  {
+    "revision": "ddb76732321fc60d4cd4",
+    "url": "/life-in-numbers/static/js/2.535d8d75.chunk.js"
+  },
+  {
+    "revision": "3505a3c1a6769c9875adb5d5d33cb20a",
+    "url": "/life-in-numbers/static/js/2.535d8d75.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5cfb51ff7d45924c55c",
+    "url": "/life-in-numbers/static/js/main.8f5a7fa8.chunk.js"
+  },
+  {
+    "revision": "57edf39bab6948ce1f16",
+    "url": "/life-in-numbers/static/js/runtime-main.78da8841.js"
+  }
+]);
